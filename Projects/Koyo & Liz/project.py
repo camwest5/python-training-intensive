bb6264624656bd86765120c53207de2f633b395d
camwest5 <118239146+camwest5@users.noreply.github.com>
2025-01-16 15:04:03 +1000

    Fix f-string again

--- Projects/Koyo & Liz/project.py
+++ Projects/Koyo & Liz/project.py
@@ -245,7 +245,7 @@ state_with_max_count
 #| title: "Which phase of flight sees highest likelihood of birds strike?"
 #| icon: rulers
 #| color: secondary
-"Approach : " + df_filtered["phase_of_flt"].count()
+"Approach : " + str(df_filtered["phase_of_flt"].count())
 
 
 # %%
