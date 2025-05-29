5702340dcbc47a2a72d0b4689b47f8cd22a98003
camwest5 <118239146+camwest5@users.noreply.github.com>
2025-05-29 14:13:53 +1000

    Move projects into archive folder

---
+++ Projects/summer25/Caira/dashboardbooks.py
@@ -0,0 +1,139 @@
+# %% [markdown]
+# ---
+# title: Goodreads
+# author: Caira
+# date: 2025-01-16 
+# format: 
+#    dashboard: 
+#           theme: solar 
+#           scrolling: true
+# --- 
+
+# %%
+
+import pandas as pd 
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.io as pio
+
+# %%
+
+book_raw = pd.read_csv("../../data_sources/books.csv")
+
+book = book_raw.copy() 
+
+
+# %% 
+
+#| output: false
+
+book["publication_date"] = pd.to_datetime(book["publication_date"])
+
+
+
+book["sum"] = book["ratings_count"] * book["average_rating"]
+
+book["rating_range"] =  pd.cut(book["average_rating"], [-1,0.00,0.99, 1,1.99, 2,2.99, 3,3.99, 4,4.99, 5])
+
+# %% [markdown]
+
+"""
+## Row {.fill}
+"""
+
+# %%
+
+
+#| title: "How the Publication Year of Books has an Influence on its GoodReads Ratings."
+
+px.scatter(data_frame = book, x = "publication_date", y = "sum", color = 'rating_range', 
+            hover_name = "title", hover_data = ["average_rating", "authors"],
+            labels={
+                     "publication_date": "Publication Year",
+                     "sum": "Sum of Ratings",
+                     "rating_range": "Range"
+                 })
+
+# %% [markdown]
+
+"""
+## Row {.fill}
+"""
+
+
+# %% [markdown]
+
+"""
+## Table
+"""
+
+# %%
+
+#| title: "A glimpse of the data"
+book.head(4) 
+
+
+# %% [markdown]
+
+"""
+## Row {.fill}
+"""
+
+
+# %%
+
+#| content: valuebox
+#| title: "Launched in"
+#| icon: cake-fill
+#| color: light
+#| font-size: 1
+"January 2007"
+
+# %% [markdown]
+
+"""
+## Row {.flow}
+"""
+
+
+# %%
+
+#| content: valuebox
+#| title: "Founded By"
+#| icon: pencil-square
+#| color: light
+"Otis Chandler"
+
+# %%
+
+#| content: valuebox
+#| title: "and"
+#| icon: pencil-square
+#| color: light
+"Elizabeth Khuri Chandler"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Projects/summer25/Lydeth/Goodread.py
@@ -0,0 +1,71 @@
+#%% [markdown]
+# ---
+# title: The popular author
+# author: Goodread  
+# date: 2025-01-16
+# format: 
+#   dashboard: 
+#       theme: sketchy
+# --- 
+# %%
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import datetime as dt
+df_raw = pd.read_csv('books.csv')
+
+df = df_raw.copy()
+
+df["publication_date"] = pd.to_datetime(df['publication_date'])
+# %% [markdown]
+
+"""
+## Row {.fill}
+"""
+
+# %%
+df['rating_range'] = pd.cut(df['average_rating'],[0,0.99,1,1.99,2,2.99,3,3.9,4,5])
+px.scatter(data_frame= df, x='publication_date', 
+           y='average_rating', 
+           color='rating_range',
+           labels= {'publication_date':'Publication year',
+                    'average_rating': 'Star rating',
+                    'rating_range': 'Range'},
+           title= 'Rating popularity')
+
+# %% [markdown]
+
+"""
+## Row {.fill}
+"""
+# %%
+#| title: 'Average rating base on year'
+df_filtered = df[df['average_rating'] > 4.5]
+sns.relplot(data=df_filtered, x='publication_date', y='average_rating', kind= 'line')
+
+# %%
+#| title: 'Top publishers'
+df_filtered = df[df['average_rating'] == 5.0]
+df_new=df_filtered[['publisher']]
+df_new
+# %%
+#| content: valuebox
+#| title: 'Number of books that got 5 stars'
+#| icon: book 
+#| color: primary 
+22
+# %%
+#| content: valuebox
+#| title: 'Number of publishers that got the highest rated book'
+#| icon: trophy-fill
+#| color: secondary 
+13
+
+
+
+
+
+
+
